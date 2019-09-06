76db57cd850db8aef199f0d908984c0bdc8ebfe0
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-09-05 20:33:19 -0700

    Add CI/CD in CDK

--- static-love-cdk/build.gradle.kts
+++ static-love-cdk/build.gradle.kts
@@ -8,6 +8,9 @@ dependencies {
     implementation("software.amazon.awscdk:s3:1.6.0.DEVPREVIEW")
     implementation("software.amazon.awscdk:route53:1.6.0.DEVPREVIEW")
     implementation("software.amazon.awscdk:route53-targets:1.6.0.DEVPREVIEW")
+    implementation("software.amazon.awscdk:codepipeline:1.6.0.DEVPREVIEW")
+    implementation("software.amazon.awscdk:codebuild:1.6.0.DEVPREVIEW")
+    implementation("software.amazon.awscdk:codepipeline-actions:1.6.0.DEVPREVIEW")
 
     // Use Junit test framework
     testImplementation("junit:junit:4.12")
--- static-love-cdk/src/main/java/com/zzx/love/cdk/StaticLoveStack.java
+++ static-love-cdk/src/main/java/com/zzx/love/cdk/StaticLoveStack.java
@@ -1,12 +1,39 @@
 package com.zzx.love.cdk;
 
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.SecretValue;
+import software.amazon.awscdk.core.SecretsManagerSecretOptions;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.codebuild.BuildEnvironment;
+import software.amazon.awscdk.services.codebuild.BuildSpec;
+import software.amazon.awscdk.services.codebuild.ComputeType;
+import software.amazon.awscdk.services.codebuild.GitHubSourceProps;
+import software.amazon.awscdk.services.codebuild.LinuxBuildImage;
+import software.amazon.awscdk.services.codebuild.PipelineProject;
+import software.amazon.awscdk.services.codebuild.PipelineProjectProps;
+import software.amazon.awscdk.services.codebuild.Project;
+import software.amazon.awscdk.services.codebuild.ProjectProps;
+import software.amazon.awscdk.services.codebuild.Source;
+import software.amazon.awscdk.services.codepipeline.Artifact;
+import software.amazon.awscdk.services.codepipeline.IAction;
+import software.amazon.awscdk.services.codepipeline.IStage;
+import software.amazon.awscdk.services.codepipeline.Pipeline;
+import software.amazon.awscdk.services.codepipeline.PipelineProps;
+import software.amazon.awscdk.services.codepipeline.StageProps;
+import software.amazon.awscdk.services.codepipeline.actions.CodeBuildAction;
+import software.amazon.awscdk.services.codepipeline.actions.CodeBuildActionProps;
+import software.amazon.awscdk.services.codepipeline.actions.CodeBuildActionType;
+import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceAction;
+import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceActionProps;
 import software.amazon.awscdk.services.iam.AnyPrincipal;
 import software.amazon.awscdk.services.iam.Effect;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
 import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.iam.PolicyStatementProps;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.RoleProps;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.route53.ARecord;
 import software.amazon.awscdk.services.route53.ARecordProps;
 import software.amazon.awscdk.services.route53.HostedZone;
@@ -16,6 +43,7 @@ import software.amazon.awscdk.services.route53.targets.BucketWebsiteTarget;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.BucketProps;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -35,6 +63,7 @@ public class StaticLoveStack extends Stack {
     private void init() {
         Bucket bucket = createStaticWebsiteBucket();
         createRoute53Record(bucket);
+        createCodeBuildProject();
     }
 
     private Bucket createStaticWebsiteBucket() {
@@ -80,6 +109,51 @@ public class StaticLoveStack extends Stack {
 
     // CI/CD system for deploying static content to S3
     private void createCodeBuildProject() {
+        IAction githubSourceAction = new GitHubSourceAction(GitHubSourceActionProps.builder()
+            .actionName("Source")
+            .owner("jokyjang")
+            .repo("ZhangZhaoxiBe")
+            .branch("master")
+            .oauthToken(SecretValue.secretsManager("jokyjang.github.oauth", SecretsManagerSecretOptions.builder()
+                .jsonField("zhang.zhaoxi.be.oauth.token")
+                .build()))
+            .output(Artifact.artifact("Source"))
+            .build());
+        StageProps sourceStage = StageProps.builder()
+            .stageName("Source")
+            .actions(Collections.singletonList(githubSourceAction))
+            .build();
 
+        IAction codeBuildAction = new CodeBuildAction(CodeBuildActionProps.builder()
+            .actionName("Build")
+            .input(Artifact.artifact("Source"))
+            .project(new PipelineProject(this, "StaticLoveProject", PipelineProjectProps.builder()
+                .buildSpec(BuildSpec.fromSourceFilename("./static-love/buildspec.yml"))
+                .projectName("StaticLoveProjectBuild")
+                .environment(BuildEnvironment.builder()
+                    .computeType(ComputeType.SMALL)
+                    .buildImage(LinuxBuildImage.STANDARD_2_0)
+                    // TODO add environment variables for target s3 bucket
+                    .build())
+                .role(new Role(this, "StaticLoveProjectRole", RoleProps.builder()
+                    .roleName("StaticLoveProjectRole")
+                    .assumedBy(new ServicePrincipal("codebuild.amazonaws.com"))
+                    .managedPolicies(Arrays.asList(
+                        ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+                        ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess")))
+                    .build()))
+                .build()))
+            .build());
+
+        StageProps buildStage = StageProps.builder()
+            .stageName("Build")
+            .actions(Collections.singletonList(codeBuildAction))
+            .build();
+
+        new Pipeline(this, "StaticLovePipeline", PipelineProps.builder()
+            .pipelineName("StaticLove")
+            .stages(Arrays.asList(sourceStage, buildStage))
+            .build()
+        );
     }
 }
